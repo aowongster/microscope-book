7410917f9c8dbd17a819a22b6684e76c9dc6b8db
Alistair Wong <aowongster@gmail.com>
2016-12-10 17:27:49 -0800

    add basic edit functionality

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -5,7 +5,17 @@ Template.postSubmit.events({
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val()
         };
-        post._id = Posts.insert(post);
-        Router.go('postPage', post);
+
+        // call server side meteor method to do insert
+        Meteor.call('postInsert', post, function(error, result) { // display the error to the user and abort
+            if (error)
+                return alert(error.reason);
+
+            // catch special case and alert user
+            if (result.postExists)
+                alert('This link has already been posted');
+                
+            Router.go('postPage', {_id: result._id});
+        });
     }
 });
