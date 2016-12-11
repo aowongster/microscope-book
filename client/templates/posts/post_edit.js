a761e47e4aaf28b0820088d0cdc97041f9ade255
Alistair Wong <aowongster@gmail.com>
2016-12-11 10:05:14 -0800

    add meteor edit duplicate check

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -4,17 +4,20 @@ Template.postEdit.events({
         var currentPostId = this._id;
         var postProperties = {
             url: $(e.target).find('[name=url]').val(),
-            title: $(e.target).find('[name=title]').val()
-        }
-        Posts.update(currentPostId, {
-            $set: postProperties
-        }, function(error) {
-            if (error) {
-                // display the error to the user
-                alert(error.reason);
-            } else {
-                Router.go('postPage', {_id: currentPostId});
-            }
+            title: $(e.target).find('[name=title]').val(),
+            _id: currentPostId
+        };
+
+        // call server side meteor method to do insert
+        Meteor.call('postEdit', postProperties, function(error, result) { // display the error to the user and abort
+            if (error)
+                return alert(error.reason);
+
+            // catch special case and alert user
+            if (result.postExists)
+                alert('This link has already been posted');
+
+            Router.go('postPage', {_id: result._id});
         });
     },
     'click .delete': function(e) {
