029514c187c42af177aef7d597ab7c272c60b67a
Alistair Wong <aowongster@gmail.com>
2016-12-23 11:42:11 -0800

    add comments pre select

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,8 @@
+// controll what the server publishes to client
 Meteor.publish('posts', function() {
   return Posts.find();
 });
-Meteor.publish('comments', function() {
-  return Comments.find();
+Meteor.publish('comments', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
 });
